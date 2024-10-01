cbaba6d389dd2a87c49202009c3ca891dcef3ff7
Mayon Francis <mayon.francis@smallcase.com>
2024-10-01 16:00:40 +0530

    runtime params

--- dags/2.py
+++ dags/2.py
@@ -1,8 +1,10 @@
-from datetime import datetime
+# from datetime import datetime
 from airflow import DAG
-from airflow.providers.cncf.kubernetes.operators.job import KubernetesJobOperator
+# from airflow.providers.cncf.kubernetes.operators.job import KubernetesJobOperator
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from airflow.utils.dates import days_ago
+from airflow.models.param import Param
+import shlex
 
 # Define a simple DAG
 default_args = {
@@ -10,19 +12,46 @@ default_args = {
     'start_date': days_ago(1),
 }
 
+params = {
+    "yourName": Param(
+        default=0,
+        description="Enter your name please",
+        type="string"
+    ),
+    "userIds": Param(
+        default=0,
+        description="Enter user ids to run for",
+        type="string"
+    ),
+    "obj": Param(
+        default=0,
+        description="Enter an obj",
+        type="object"
+    ),
+    "myArr": Param(
+        default=0,
+        description="Enter new line scids",
+        type="array"
+    ),
+}
+
 with DAG(
     dag_id='test-kube-pod-dag',
     default_args=default_args,
     schedule_interval=None,
     catchup=False,
+    # render_template_as_native_obj=True,
+    params = params
 ) as dag:
 
     # Kubernetes Pod Operator Task
     kubernetes_task = KubernetesPodOperator(
         image='deposit-job:latest',
-        # cmds=["python", "-c"],
+        cmds=["sh", "-c"],
+        arguments=[
+            f"npm run start-job -- --params={shlex.quote("{{ params | tojson }}")}"
+        ],
         task_id="deposit-task",
-        # arguments=["print('Hello from Airflow Kubernetes Pod Operator!')"],
         get_logs=True,
         in_cluster=True,
 
@@ -33,5 +62,5 @@ with DAG(
 
         image_pull_policy="IfNotPresent",
 
-        on_finish_action='delete_succeeded_pod'
+        on_finish_action='keep_pod',
     )
